11b60eb7b3931e617b40409386701bbd84ca9b0d
JonhIsh <rodriguezjonhy04@gmail.com>
2025-11-11 23:28:26 +0000

    Added check build stage to github checks, placeholder nav, home and i18n

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+})
+export class HomeComponent {
+
+}
