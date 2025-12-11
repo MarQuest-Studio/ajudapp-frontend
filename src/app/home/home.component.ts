b83f3b5fa55a43ab03b4d870ac8a4c750f1979ff
JonhIsh <rodriguezjonhy04@gmail.com>
2025-12-11 20:08:49 +0000

    Fully remade home page, fixed action types

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectDistricts } from '../state/regions/regions.selector';
-import { loadRegions } from '../state/regions/regions.actions';
+import { loadRegions, searchRegions } from '../state/regions/regions.actions';
+import { DropdownFilterComponent } from "../shared/dropdown-filter/dropdown-filter.component";
+import { RegionHit } from '../core/services/regions.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, DropdownFilterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
@@ -22,5 +24,40 @@ export class HomeComponent implements OnInit {
       console.log(districts);
     });
     this.store.dispatch(loadRegions());
+  }
+
+  onDistrictSelected($event: RegionHit) {
+    console.log($event);
+  }
+
+  onLocationSearch(searchTerm: string) {
+    console.log('Searching for:', searchTerm);
+    this.store.dispatch(searchRegions({query: searchTerm}));
+  }
+
+  getLabel(hit: RegionHit): string {
+    switch (hit.kind) {
+      case 'district':
+        return hit.district;
+      case 'city':
+        return `${hit.city}, ${hit.district}`;
+      case 'parish':
+        return `${hit.parish}, ${hit.city}, ${hit.district}`;
+      default:
+        return hit.district;
+    }
+  }
+
+  getHitKind(hit: RegionHit): string {
+    switch (hit.kind) {
+      case 'district':
+        return 'Distrito';
+      case 'city':
+        return 'Concelho';
+      case 'parish':
+        return 'Freguesia';
+      default:
+        return hit.district;
+    }
   }
 }
